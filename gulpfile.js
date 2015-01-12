0ac6465568dcccb8d39509551f7e93b9cab5e883
Austin McDaniel <amcdaniel2@gmail.com>
2015-01-12 17:51:23 -0500

    builds but doesn't run :(

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var assign = Object.assign || require('object.assign');
 var sourcemaps = require("gulp-sourcemaps");
 var ngHtml2Js = require("gulp-ng-html2js");
 var htmlMin = require('gulp-minify-html');
+var builder = require('systemjs-builder');
+var RSVP = require('rsvp');
 
 var compilerOptions = {
   filename: '',
@@ -73,14 +75,12 @@ gulp.task('build-html', function () {
         });
       }
     }))*/
-    .pipe(ngHtml2Js(
-      //moduleName: 'templates'
-    ))
+    .pipe(ngHtml2Js())
     .pipe(gulp.dest(path.output))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('build-system', function () {
+gulp.task('build-es6', function () {
   return gulp.src(path.source)
     .pipe(plumber())
     .pipe(changed(path.output, {extension: '.js'}))
@@ -91,10 +91,10 @@ gulp.task('build-system', function () {
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('build', function(callback) {
+gulp.task('watch-build', function(callback) {
   return runSequence(
     'clean',
-    ['build-html', 'build-system'],
+    ['build-html', 'build-es6'],
     callback
   );
 });
@@ -105,7 +105,7 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('serve', ['build'], function(done) {
+gulp.task('serve', ['watch-build'], function(done) {
   browserSync({
     open: false,
     port: 9000,
@@ -120,11 +120,61 @@ gulp.task('serve', ['build'], function(done) {
 });
 
 gulp.task('watch', ['serve'], function() {
-  var watcher = gulp.watch([path.source, path.html], ['build']);
+  var watcher = gulp.watch([path.source, path.html], ['watch-build']);
   watcher.on('change', function(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
 });
 
-// todo:
-// gulp.task('build', []);
+gulp.task('build', runSequence('clean', ['build-html', 'build-es6'], function() {
+
+  var routes = require('./src/app/routes.json');
+
+  // inject first
+  routes.unshift({
+    src: "src/app/app"
+  });
+
+  builder.loadConfig('./system.config.js').then(function(){
+
+    var promises = [];
+
+    routes.forEach(function(t){
+
+      var trees = [];
+      promises.push(new RSVP.Promise(function(resolve, reject) {
+        console.log('+ Tracing:', t.src);
+
+        builder.trace(t.src).then(function(traceTree) {
+          trees.push({
+            src: t.src, 
+            tree: traceTree.tree
+          });
+          resolve();
+        });
+
+      }));
+
+      RSVP.all(promises).then(function(){
+        trees.forEach(function(tree){
+
+          trees.forEach(function(childTree){
+            if(childTree !== tree){
+              builder.subtractTrees(tree.tree, childTree.tree);
+            }
+          });
+
+          console.log('+ Building tree:', tree.src)
+
+          // build the tree
+          var src = tree.src.replace('src', 'dist');
+          builder.buildTree(tree.tree, src + '.js', { sourceMaps: true });
+        });
+
+      });
+
+    });
+
+  });
+
+}));
