7d53cb12bdeb0ec9f339a8d78c6338c4c06189de
Austin McDaniel <amcdaniel2@gmail.com>
2015-01-15 19:38:15 -0500

    deps are now mapped right

--- build/build.js
+++ build/build.js
@@ -2,25 +2,47 @@ var to5 = require('gulp-6to5');
 var builder = require('systemjs-builder');
 var RSVP = require('rsvp');
 
+/**
+ * Recursively traces all the deps
+ */
+var traceDeps = function(parents, cb){
+  var count = 0, 
+    trees = [], 
+    trace = function(nodes){
+      nodes.forEach(function(node){
+        count++;
+        builder.trace(node).then(function(tree){
+          trees.push(tree);
 
-var trace = function(nodes){
+          var keys = Object.keys(tree.tree);
+          if(keys.length){
+            var dedeps = [];
+            keys.forEach(function(k){
+              var name = tree.tree[k].name;
 
-  nodes.forEach(function(node){
+              // bug or something, dedups itself somtimes
+              if(name === tree.moduleName) {
+                delete tree.tree[name];
+                return;
+              }
 
-    builder.trace(node)then(function(tree){
-      
-      if(tree.deps && tree.deps.length){
-        trace(tree.deps);
-      }
+              // find unique modules only ....
+              if(!trees.find(function(t){ return t.moduleName === name; })){
+                dedeps.push(name);
+              }
+            });
+            trace(dedeps);
+          }
 
-    });
-
-  });
+          count--;
+          if (count === 0) cb(trees);
+        });
+      });
+    };
+  trace(parents);
 };
 
-
 /**
- *
  *  config = {
  *    main: 'app/app.js',
  *    config: systemjs config,
@@ -30,28 +52,18 @@ var trace = function(nodes){
 var build = function(config){
 
   builder.loadConfig(config.config).then(function(that){
-    debugger;
 
-    trace(config.bundles).then(function(nodes){
+    traceDeps([config.main], function(main){
 
+      traceDeps(config.bundles, function(bundles){
 
-
-    });
-
-    /*var promises = [];
-
-    config.bundles.forEach(function(t){
-      promises.push(builder.trace(t));
-    });
-
-    RSVP.all(promises).then(function(trees){
-
-      var nodes = [];
-      trees.forEach(function(tree){
+        // flattens all our deps so we can loop commons together
+        // results: http://www.screencast.com/t/bV16I8bc2PF
+        debugger;
 
       });
 
-    });*/
+    });
 
   });
 };
