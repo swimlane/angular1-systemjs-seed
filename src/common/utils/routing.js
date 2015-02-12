a29ddf9a52ce6482eb9dc67f32463380473144fe
Jon Stevens <latchkey@gmail.com>
2015-02-12 01:29:14 -0800

    fix formatting and duplicate var declaration error

--- src/common/utils/routing.js
+++ src/common/utils/routing.js
@@ -14,12 +14,12 @@ export var routing = function(module){
 
       System.import(futureState.src).then(loaded => {
         var newModule = loaded;
-        if(!loaded.name){
-          var key = Object.keys(loaded), 
+        if (!loaded.name) {
+          var key = Object.keys(loaded);
           newModule = loaded[key[0]];
         }
         
-        $ocLazyLoad.load(newModule).then(function(){
+        $ocLazyLoad.load(newModule).then(function() {
           def.resolve();
         });
       });
@@ -30,7 +30,6 @@ export var routing = function(module){
     futureRoutes.forEach(function(r){
       $futureStateProvider.futureState(r);
     });
-    
   };
 
   RouterConfig.$inject = $inject;
