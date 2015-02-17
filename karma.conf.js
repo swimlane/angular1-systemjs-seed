d3c5f9b9b834a5971a7c30bf51b275283952fbeb
Marjan <marjan@cognitelabs.com>
2015-02-17 08:13:19 +0100

    Fix karma config basePath

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: 'dist',
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jspm', 'jasmine'],
