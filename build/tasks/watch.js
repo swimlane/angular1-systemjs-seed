ef76e98821619ec715fb7e54ce2c7f34dffeab59
Austin <amcdaniel2@gmail.com>
2015-09-09 08:48:10 -0400

    Update watch.js

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -9,5 +9,5 @@ function changed(event) {
 gulp.task('watch', ['build'], function () {
   gulp.watch([ paths.source ], [ 'es6', browserSync.reload ]).on('change', changed);
   gulp.watch([ paths.html ], [ 'html', browserSync.reload ]).on('change', changed);
-  gulp.watch([ paths.less ], [ 'less-dev' ]).on('change', changed);
+  gulp.watch([ paths.less ], [ 'less' ]).on('change', changed);
 });
