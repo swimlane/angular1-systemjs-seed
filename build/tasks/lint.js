6c57570822f562e97d31888cde5fcf237796794f
Georgios Diamantopoulos <georgiosd@live.com>
2015-09-25 12:52:37 +0300

    Fix lint task

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var paths = require('../paths');
 
 gulp.task('lint', function () {
-  return gulp.src(path.source)
+  return gulp.src(paths.source)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
